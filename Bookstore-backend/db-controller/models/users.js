71b3ae999a1ceefee74dacd600a3a639903d6344
SanjitKumarr <sanjit.kumar@triconinfotech.com>
2022-12-23 20:59:06 +0530

    added register and login route

--- Bookstore-backend/db-controller/models/users.js
+++ Bookstore-backend/db-controller/models/users.js
@@ -2,18 +2,11 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 let User =new Schema({ 
-    userId:{
-        type:String,
-    },
-    userName:{
-        type:String,
-    },
-    firstName:{
-        type:String,
-    },
-    lastName:{
-        type:String,
-    },
+    firstName: { type: String, default: null },
+    lastName: { type: String, default: null },
+    email: { type: String, unique: true },
+    password: { type: String },
+    token: { type: String },
 },
 {
     collection:'users'
